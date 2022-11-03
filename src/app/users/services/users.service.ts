5825276b37d2a40bda72ac5badc5c4391249dc9c
ogimly <ogimly@gmail.com>
2022-11-03 22:08:19 +0500

    feat: add methods to user.service

--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -1,9 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { APIEndpoints } from '@core/enums/api-endpoints.enum';
+import { Observable } from 'rxjs';
+import { SignUpParams, User } from 'src/app/auth/models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
   constructor(private http: HttpClient) {}
+
+  public getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(APIEndpoints.users);
+  }
+
+  public getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${APIEndpoints.users}/${id}`);
+  }
+
+  public updUser(id: string, userParams: SignUpParams): Observable<User> {
+    return this.http.put<User>(APIEndpoints.signUp, userParams);
+  }
+
+  public delUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${APIEndpoints.users}/${id}`);
+  }
 }
