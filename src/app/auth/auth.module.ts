70331eb2364514d7ce03de6c8fef6c5345e8f93e
Vadzim Antonau <vanet@tut.by>
2022-11-04 23:03:51 +0300

    feat: add store for User Sign In

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { UserEffects } from '../store/effects/user.effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([UserEffects])],
 })
 export class AuthModule {}
---
+++ src/app/store/effects/user.effects.spec.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { UserEffects } from './user.effects';
+
+describe('UserEffects', () => {
+  let actions$: Observable<Action>;
+  let effects: UserEffects;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserEffects, provideMockActions(() => actions$), HttpClient, HttpHandler],
+    });
+
+    effects = TestBed.inject(UserEffects);
+  });
+
+  it('should be created', () => {
+    expect(effects).toBeTruthy();
+  });
+});
