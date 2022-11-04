0ed7a63a61777b7f85fb8eb861f5fd97a7bbde78
ogimly <ogimly@gmail.com>
2022-11-04 14:20:42 +0500

    fix: naming

--- src/app/boards/services/boards.service.ts
+++ src/app/boards/services/boards.service.ts
@@ -22,11 +22,11 @@ export class BoardsService {
     return this.http.get<Board>(`${APIEndpoints.boards}/${id}`);
   }
 
-  public updBoard(id: string, boardParams: BoardParams): Observable<Board> {
+  public updateBoard(id: string, boardParams: BoardParams): Observable<Board> {
     return this.http.put<Board>(`${APIEndpoints.boards}/${id}`, boardParams);
   }
 
-  public delBoard(id: string): Observable<Board> {
+  public deleteBoard(id: string): Observable<Board> {
     return this.http.delete<Board>(`${APIEndpoints.boards}/${id}`);
   }
 
