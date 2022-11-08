8d0747c378b70939af69f67d9e0f98019ceeef68
Zhenya <evgeegai@gmail.com>
2022-11-08 21:12:14 +0300

    fix: optimise style imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,10 @@ import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [LoginComponent, SignUpComponent],
-  imports: [CommonModule, SharedModule, NzFormModule, NzModalModule, NzSelectModule, NzDividerModule, NzCheckboxModule],
+  imports: [CommonModule, SharedModule, NzFormModule, NzModalModule],
 })
 export class AuthModule {}
