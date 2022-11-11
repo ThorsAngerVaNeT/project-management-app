994ffd6b9c2724b5f49af0b9c49c010bbc7660a5
Vadzim Antonau <vanet@tut.by>
2022-11-11 19:19:46 +0300

    refactor: actions props to be connected with models

--- src/app/boards/store/actions/board.actions.ts
+++ src/app/boards/store/actions/board.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { Update } from '@ngrx/entity';
 import { Board, BoardParams } from '../../models/board.model';
+import { User } from '../../../users/models/user.model';
 
 export const loadBoards = createAction('[Boards] Load Boards');
 export const loadBoardsSuccess = createAction('[Boards] Load Boards Success', props<{ boards: Board[] }>());
@@ -10,11 +11,11 @@ export const loadBoard = createAction('[Boards] Load Board', props<{ id: string 
 export const loadBoardSuccess = createAction('[Boards] Load Board Success', props<{ board: Board }>());
 export const loadBoardFailure = createAction('[Boards] Load Board Failure', props<{ error: unknown }>());
 
-export const loadBoardsSet = createAction('[Boards] Load BoardsSet', props<{ ids: string[] }>());
+export const loadBoardsSet = createAction('[Boards] Load BoardsSet', props<{ ids: Board['_id'][] }>());
 export const loadBoardsSetSuccess = createAction('[Boards] Load BoardsSet Success', props<{ boards: Board[] }>());
 export const loadBoardsSetFailure = createAction('[Boards] Load BoardsSet Failure', props<{ error: unknown }>());
 
-export const loadBoardsByUser = createAction('[Boards] Load BoardsByUser', props<{ userId: string }>());
+export const loadBoardsByUser = createAction('[Boards] Load BoardsByUser', props<{ userId: User['_id'] }>());
 export const loadBoardsByUserSuccess = createAction('[Boards] Load BoardsByUser Success', props<{ boards: Board[] }>());
 export const loadBoardsByUserFailure = createAction('[Boards] Load BoardsByUser Failure', props<{ error: unknown }>());
 
--- src/app/users/store/actions/user.actions.ts
+++ src/app/users/store/actions/user.actions.ts
@@ -7,7 +7,7 @@ export const loadUsers = createAction('[Users] Load Users');
 export const loadUsersSuccess = createAction('[Users] Load Users Success', props<{ users: User[] }>());
 export const loadUsersFailed = createAction('[Users] Load Users Failed', props<{ error: unknown }>());
 
-export const loadUser = createAction('[Users] Load User', props<{ id: string }>());
+export const loadUser = createAction('[Users] Load User', props<{ id: User['_id'] }>());
 export const loadUserSuccess = createAction('[Users] Load User Success', props<{ user: User }>());
 export const loadUserFailed = createAction('[Users] Load User Failed', props<{ error: unknown }>());
 
@@ -15,10 +15,10 @@ export const createUser = createAction('[Users] Create User', props<{ user: User
 export const createUserSuccess = createAction('[Users] Create User Success', props<{ user: User }>());
 export const createUserFailed = createAction('[Users] Create User Failed', props<{ error: unknown }>());
 
-export const updateUser = createAction('[Users] Update User', props<{ id: string; user: UserParams }>());
+export const updateUser = createAction('[Users] Update User', props<{ id: User['_id']; user: UserParams }>());
 export const updateUserSuccess = createAction('[Users] Update User Success', props<{ user: Update<User> }>());
 export const updateUserFailed = createAction('[Users] Update User Failed', props<{ error: unknown }>());
 
-export const deleteUser = createAction('[Users] Delete User', props<{ id: string }>());
-export const deleteUserSuccess = createAction('[Users] Delete User Success', props<{ id: string }>());
+export const deleteUser = createAction('[Users] Delete User', props<{ id: User['_id'] }>());
+export const deleteUserSuccess = createAction('[Users] Delete User Success', props<{ id: User['_id'] }>());
 export const deleteUserFailed = createAction('[Users] Delete User Failed', props<{ error: unknown }>());
