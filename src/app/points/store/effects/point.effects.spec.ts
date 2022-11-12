b3d881ea1837ed73f277259391dfd871b2430184
Vadzim Antonau <vanet@tut.by>
2022-11-12 14:44:10 +0300

    fix: add HttpClientTestingModule to PointEffects test

--- src/app/points/store/effects/point.effects.spec.ts
+++ src/app/points/store/effects/point.effects.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
@@ -11,6 +12,7 @@ describe('PointEffects', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [PointEffects, provideMockActions(() => actions$)],
     });
 
