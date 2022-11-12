6d0e7610bf739c7ba3327ad78c737339aa9cf0b0
Vadzim Antonau <vanet@tut.by>
2022-11-12 10:59:09 +0300

    feat: add effects for Points Store

--- src/app/points/store/effects/point.effects.ts
+++ src/app/points/store/effects/point.effects.ts
@@ -1,23 +1,83 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
-import { EMPTY, of } from 'rxjs';
+import { of } from 'rxjs';
 import * as PointActions from '../actions/point.actions';
+import { PointsService } from '../../services/points.service';
 
 @Injectable()
 export class PointEffects {
-  loadPoints$ = createEffect(() => {
+  constructor(private actions$: Actions, private pointsService: PointsService) {}
+
+  loadPointsSet$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(PointActions.loadPoints),
-      concatMap(() =>
-        /** An EMPTY observable only emits completion. Replace with your own observable API request */
-        EMPTY.pipe(
-          map((data) => PointActions.loadPointsSuccess({ data })),
-          catchError((error) => of(PointActions.loadPointsFailure({ error }))),
+      ofType(PointActions.loadPointsSet),
+      concatMap(({ ids }) =>
+        this.pointsService.getPointsSet(ids).pipe(
+          map((points) => PointActions.loadPointsSetSuccess({ points })),
+          catchError((error) => of(PointActions.loadPointsSetFailure({ error }))),
         ),
       ),
     );
   });
 
-  constructor(private actions$: Actions) {}
+  loadPointsByUser$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PointActions.loadPointsByUser),
+      concatMap(({ userId }) =>
+        this.pointsService.getPointsByUser(userId).pipe(
+          map((points) => PointActions.loadPointsByUserSuccess({ points })),
+          catchError((error) => of(PointActions.loadPointsByUserFailure({ error }))),
+        ),
+      ),
+    );
+  });
+
+  loadPointsByTask$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PointActions.loadPointsByTask),
+      concatMap(({ taskId }) =>
+        this.pointsService.getPointsByTask(taskId).pipe(
+          map((points) => PointActions.loadPointsByTaskSuccess({ points })),
+          catchError((error) => of(PointActions.loadPointsByTaskFailure({ error }))),
+        ),
+      ),
+    );
+  });
+
+  createPoint$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PointActions.createPoint),
+      concatMap(({ point: pointParams }) =>
+        this.pointsService.createPoint(pointParams).pipe(
+          map((point) => PointActions.createPointSuccess({ point })),
+          catchError((error) => of(PointActions.createPointFailure({ error }))),
+        ),
+      ),
+    );
+  });
+
+  updatePoint$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PointActions.updatePoint),
+      concatMap(({ pointId, pointParams }) =>
+        this.pointsService.updatePoint(pointId, pointParams).pipe(
+          map((point) => PointActions.updatePointSuccess({ point })),
+          catchError((error) => of(PointActions.updatePointFailure({ error }))),
+        ),
+      ),
+    );
+  });
+
+  updatePointsSet$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PointActions.updatePointsSet),
+      concatMap(({ pointsParams }) =>
+        this.pointsService.updatePointsSet(pointsParams).pipe(
+          map((points) => PointActions.updatePointsSetSuccess({ points })),
+          catchError((error) => of(PointActions.updatePointsSetFailure({ error }))),
+        ),
+      ),
+    );
+  });
 }
