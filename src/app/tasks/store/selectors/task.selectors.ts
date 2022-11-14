7fe228ca2d0929d3eab768a227ec3716ee68d705
Vadzim Antonau <vanet@tut.by>
2022-11-14 19:20:50 +0300

    feat: add tasks of current board grouped by column selector

--- src/app/tasks/store/selectors/task.selectors.ts
+++ src/app/tasks/store/selectors/task.selectors.ts
@@ -1,6 +1,21 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromTask from '../reducers/task.reducer';
+import { selectBoardId } from '@core/store/selectors/router.selector';
+import { ColumnTask } from '../../model/task.model';
 
 export const selectTaskState = createFeatureSelector<fromTask.TasksState>(fromTask.tasksFeatureKey);
 
 export const selectAllTasks = createSelector(selectTaskState, fromTask.selectAllTasks);
+
+export const selectTasksOfCurrentBoardByColumns = createSelector(selectAllTasks, selectBoardId, (tasks, boardId) => {
+  const groupedTasks: { [keyof: string]: ColumnTask[] } = {};
+  const currentBoardTasks = tasks
+    .filter((task) => task.boardId === boardId)
+    .reduce((acc, cur) => {
+      if (!acc[cur.columnId]) acc[cur.columnId] = [];
+      acc[cur.columnId].push(cur);
+      return acc;
+    }, groupedTasks);
+
+  return currentBoardTasks;
+});
