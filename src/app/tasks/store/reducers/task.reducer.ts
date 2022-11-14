74911efac725d1613fcc8ba9555771280d1962a7
Vadzim Antonau <vanet@tut.by>
2022-11-14 15:00:15 +0300

    feat: add reducers for Tasks Store

--- src/app/tasks/store/reducers/task.reducer.ts
+++ src/app/tasks/store/reducers/task.reducer.ts
@@ -1,16 +1,28 @@
 import { createReducer, on } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+import { ColumnTask } from '../../model/task.model';
 import * as TaskActions from '../actions/task.actions';
 
-export const taskFeatureKey = 'task';
+export const tasksFeatureKey = 'tasks';
 
-export interface State {}
+export interface TasksState extends EntityState<ColumnTask> {}
 
-export const initialState: State = {};
+export const adapter: EntityAdapter<ColumnTask> = createEntityAdapter<ColumnTask>({ selectId: (task) => task._id });
+
+export const initialState: TasksState = adapter.getInitialState({});
 
 export const reducer = createReducer(
   initialState,
-
-  on(TaskActions.loadTasks, (state): State => state),
-  on(TaskActions.loadTasksSuccess, (state): State => state),
-  on(TaskActions.loadTasksFailure, (state): State => state),
+  on(TaskActions.loadTasksSuccess, (state, { tasks }) => adapter.setAll(tasks, state)),
+  on(TaskActions.loadTasksSetSuccess, (state, { tasks }) => adapter.setAll(tasks, state)),
+  on(TaskActions.loadTasksByUserSuccess, (state, { tasks }) => adapter.setAll(tasks, state)),
+  on(TaskActions.loadTasksByBoardSuccess, (state, { tasks }) => adapter.setAll(tasks, state)),
+  on(TaskActions.loadTasksBySearchStringSuccess, (state, { tasks }) => adapter.setAll(tasks, state)),
+  on(TaskActions.loadTaskSuccess, (state, { task }) => adapter.setOne(task, state)),
+  on(TaskActions.createTaskSuccess, (state, { task }) => adapter.addOne(task, state)),
+  on(TaskActions.updateTaskSuccess, (state, { task }) => adapter.updateOne(task, state)),
+  on(TaskActions.updateTasksSetSuccess, (state, { tasks }) => adapter.updateMany(tasks, state)),
+  on(TaskActions.deleteTask, (state, { taskId }) => adapter.removeOne(taskId, state)),
 );
+
+export const { selectIds, selectEntities, selectAll, selectTotal } = adapter.getSelectors();
