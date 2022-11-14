2522d548649f7319fbb0c31c689549d2ea7246b2
Vadzim Antonau <vanet@tut.by>
2022-11-14 11:36:52 +0300

    feat: add current board columns selector

--- src/app/columns/store/selectors/column.selectors.ts
+++ src/app/columns/store/selectors/column.selectors.ts
@@ -1,6 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { selectRouter } from '../../../core/store/selectors/router.selector';
 import * as fromColumn from '../reducers/column.reducer';
 
 export const selectColumnState = createFeatureSelector<fromColumn.ColumnsState>(fromColumn.columnsFeatureKey);
 
 export const selectAllColumns = createSelector(selectColumnState, fromColumn.selectAllColumns);
+
+export const selectColumnEntities = createSelector(selectColumnState, fromColumn.selectColumnEntities);
+
+export const selectCurrentBoardColumns = createSelector(selectAllColumns, selectRouter, (columns, router) => {
+  const { boardId } = router.state.params;
+  return columns.filter((column) => column.boardId === boardId);
+});
