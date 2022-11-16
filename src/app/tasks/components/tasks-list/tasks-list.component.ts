5ad2b074e1fdb728b3cf822dbe1dc4358d5c5d72
ogimly <ogimly@gmail.com>
2022-11-17 00:09:53 +0500

    feat: add dispatch updatetasskset on d-n-d event

--- src/app/tasks/components/tasks-list/tasks-list.component.ts
+++ src/app/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
-import { ColumnTask } from '../../model/task.model';
+import { StoreFacade } from '@core/services/store-facade/store-facade';
+import { ColumnTask, ColumnTaskSetUpdateParams } from '../../model/task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -12,9 +13,21 @@ import { ColumnTask } from '../../model/task.model';
 export class TasksListComponent {
   @Input() tasks!: ColumnTask[];
 
+  constructor(private storeFacade: StoreFacade) {}
+
   drop(event: CdkDragDrop<ColumnTask[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+
+      const columnTaskSetUpdateParams: ColumnTaskSetUpdateParams[] = event.container.data.map((task, index) => {
+        return {
+          _id: task._id,
+          order: index,
+          columnId: task.columnId,
+        };
+      });
+
+      this.storeFacade.updateTasksSet(columnTaskSetUpdateParams);
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
