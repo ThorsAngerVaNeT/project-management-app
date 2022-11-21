0646041b8814a0c9b6fbe4b7446a9ca25dcfda5d
Vadzim Antonau <vanet@tut.by>
2022-11-21 14:40:36 +0300

    feat: implement update point title functionality

--- src/app/points/components/point-item/point-item.component.ts
+++ src/app/points/components/point-item/point-item.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { StoreFacade } from '@core/services/store-facade/store-facade';
 import { Point } from '../../model/point.model';
 
@@ -13,17 +14,45 @@ export class PointItemComponent implements OnInit {
 
   isChecked!: boolean;
 
+  isEdit = false;
+
+  titleControl!: FormControl;
+
   constructor(private storeFacade: StoreFacade) {}
 
   ngOnInit(): void {
     this.isChecked = this.point.done;
+    this.titleControl = new FormControl(this.point.title, Validators.required);
+  }
+
+  toggleDone(): void {
+    this.isChecked = !this.isChecked;
+    this.updatePoint();
+  }
+
+  toggleEdit(): void {
+    this.isEdit = !this.isEdit;
+  }
+
+  createPoint(): void {
+    console.log('create');
+  }
+
+  updateTitle(): void {
+    if (this.titleControl.valid) {
+      this.toggleEdit();
+      this.updatePoint();
+    } else {
+      this.titleControl.markAsDirty();
+      this.titleControl.updateValueAndValidity({ onlySelf: true });
+    }
   }
 
   updatePoint(): void {
-    const { _id: pointId, boardId, taskId, ...pointParams } = this.point;
+    const { _id: pointId } = this.point;
+    const title = this.titleControl.value;
 
-    this.isChecked = !this.isChecked;
-    this.storeFacade.updatePoint(pointId, { ...pointParams, done: this.isChecked });
+    this.storeFacade.updatePoint(pointId, { title, done: this.isChecked });
   }
 
   deletePoint(): void {
