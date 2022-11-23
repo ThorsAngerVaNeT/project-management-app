84b24447b00e541f1b625004ea624efaf9b98b65
Vadzim Antonau <vanet@tut.by>
2022-11-23 21:52:48 +0300

    feat: add BoardUser Guard to prevent not board users access

--- src/app/boards/boards-routing.module.ts
+++ src/app/boards/boards-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardUserGuard } from '../core/guards/board-user/board-user.guard';
 import { BoardDetailComponent } from './components/board-detail/board-detail.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: ':boardId', component: BoardDetailComponent },
+  { path: ':boardId', component: BoardDetailComponent, canActivate: [BoardUserGuard], canLoad: [BoardUserGuard] },
 ];
 
 @NgModule({
