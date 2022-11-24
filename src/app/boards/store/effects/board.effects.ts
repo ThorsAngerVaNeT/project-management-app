784370c0007bb082975e4f08ef03341affcae53a
ogimly <ogimly@gmail.com>
2022-11-25 02:23:29 +0500

    refactor: del failure effects from boards store

--- src/app/boards/store/effects/board.effects.ts
+++ src/app/boards/store/effects/board.effects.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { catchError, map, concatMap, switchMap } from 'rxjs/operators';
+import { map, concatMap, switchMap } from 'rxjs/operators';
 import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import * as BoardActions from '../actions/board.actions';
 import { BoardsService } from '../../services/boards.service';
@@ -13,48 +12,32 @@ export class BoardEffects {
   loadBoards$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.loadBoards),
-      switchMap(() =>
-        this.boardsService.getBoards().pipe(
-          map((boards) => BoardActions.loadBoardsSuccess({ boards })),
-          catchError((error) => of(BoardActions.loadBoardsFailure({ error }))),
-        ),
-      ),
+      switchMap(() => this.boardsService.getBoards()),
+      map((boards) => BoardActions.loadBoardsSuccess({ boards })),
     );
   });
 
   loadBoard$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.loadBoard),
-      switchMap(({ id }) =>
-        this.boardsService.getBoard(id).pipe(
-          map((board) => BoardActions.loadBoardSuccess({ board })),
-          catchError((error) => of(BoardActions.loadBoardFailure({ error }))),
-        ),
-      ),
+      switchMap(({ id }) => this.boardsService.getBoard(id)),
+      map((board) => BoardActions.loadBoardSuccess({ board })),
     );
   });
 
   loadBoardsSet$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.loadBoardsSet),
-      switchMap(({ ids }) =>
-        this.boardsService.getBoardsSet(ids).pipe(
-          map((boards) => BoardActions.loadBoardsSetSuccess({ boards })),
-          catchError((error) => of(BoardActions.loadBoardsSetFailure({ error }))),
-        ),
-      ),
+      switchMap(({ ids }) => this.boardsService.getBoardsSet(ids)),
+      map((boards) => BoardActions.loadBoardsSetSuccess({ boards })),
     );
   });
 
   loadBoardsByUser$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.loadBoardsByUser),
-      switchMap(({ userId }) =>
-        this.boardsService.getBoardsByUser(userId).pipe(
-          map((boards) => BoardActions.loadBoardsByUserSuccess({ boards })),
-          catchError((error) => of(BoardActions.loadBoardsByUserFailure({ error }))),
-        ),
-      ),
+      switchMap(({ userId }) => this.boardsService.getBoardsByUser(userId)),
+      map((boards) => BoardActions.loadBoardsByUserSuccess({ boards })),
     );
   });
 
@@ -62,36 +45,24 @@ export class BoardEffects {
     return this.actions$.pipe(
       ofType(BoardActions.createBoard),
       concatLatestFrom(() => this.storeFacade.user$),
-      concatMap(([action, { _id: owner }]) =>
-        this.boardsService.createBoard({ ...action.board, owner }).pipe(
-          map((board) => BoardActions.createBoardSuccess({ board })),
-          catchError((error) => of(BoardActions.createBoardFailure({ error }))),
-        ),
-      ),
+      concatMap(([action, { _id: owner }]) => this.boardsService.createBoard({ ...action.board, owner })),
+      map((board) => BoardActions.createBoardSuccess({ board })),
     );
   });
 
   updateBoard$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.updateBoard),
-      concatMap((action) =>
-        this.boardsService.updateBoard(`${action.id}`, action.board).pipe(
-          map(({ _id: id, ...changes }) => BoardActions.updateBoardSuccess({ board: { id, changes } })),
-          catchError((error) => of(BoardActions.updateBoardFailure({ error }))),
-        ),
-      ),
+      concatMap((action) => this.boardsService.updateBoard(`${action.id}`, action.board)),
+      map(({ _id: id, ...changes }) => BoardActions.updateBoardSuccess({ board: { id, changes } })),
     );
   });
 
   deleteBoard$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BoardActions.deleteBoard),
-      concatMap(({ id }) =>
-        this.boardsService.deleteBoard(id).pipe(
-          map(() => BoardActions.deleteBoardSuccess({ id })),
-          catchError((error) => of(BoardActions.deleteBoardFailure({ error }))),
-        ),
-      ),
+      concatMap(({ id }) => this.boardsService.deleteBoard(id)),
+      map(({ _id: id }) => BoardActions.deleteBoardSuccess({ id })),
     );
   });
 }
