432907a8840fa76d1e19920dde3690bd4b606dd8
ogimly <ogimly@gmail.com>
2022-11-25 20:28:53 +0500

    refactor: del failure actions and effects from files store

--- src/app/files/store/effects/file.effects.ts
+++ src/app/files/store/effects/file.effects.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap, switchMap, filter } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { map, concatMap, switchMap, filter } from 'rxjs/operators';
 import * as FileActions from '../actions/file.actions';
 import { FilesService } from '../../services/files.service';
 import { environment } from '@environments/environment';
@@ -13,72 +12,48 @@ export class FileEffects {
   loadFilesSet$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.loadFilesSet),
-      switchMap(({ taskFileIds }) =>
-        this.filesService.getFilesSet(taskFileIds).pipe(
-          map((files) => FileActions.loadFilesSetSuccess({ files })),
-          catchError((error) => of(FileActions.loadFilesSetFailure({ error }))),
-        ),
-      ),
+      switchMap(({ taskFileIds }) => this.filesService.getFilesSet(taskFileIds)),
+      map((files) => FileActions.loadFilesSetSuccess({ files })),
     );
   });
 
   loadFilesByUser$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.loadFilesByUser),
-      switchMap(({ userId }) =>
-        this.filesService.getFilesByUser(userId).pipe(
-          map((files) => FileActions.loadFilesByUserSuccess({ files })),
-          catchError((error) => of(FileActions.loadFilesByUserFailure({ error }))),
-        ),
-      ),
+      switchMap(({ userId }) => this.filesService.getFilesByUser(userId)),
+      map((files) => FileActions.loadFilesByUserSuccess({ files })),
     );
   });
 
   loadFilesByTask$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.loadFilesByTask),
-      switchMap(({ taskId }) =>
-        this.filesService.getFilesByTask(taskId).pipe(
-          map((files) => FileActions.loadFilesByTaskSuccess({ files })),
-          catchError((error) => of(FileActions.loadFilesByTaskFailure({ error }))),
-        ),
-      ),
+      switchMap(({ taskId }) => this.filesService.getFilesByTask(taskId)),
+      map((files) => FileActions.loadFilesByTaskSuccess({ files })),
     );
   });
 
   loadFilesByBoard$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.loadFilesByBoard),
-      switchMap(({ boardId }) =>
-        this.filesService.getFilesByBoard(boardId).pipe(
-          map((files) => FileActions.loadFilesByBoardSuccess({ files })),
-          catchError((error) => of(FileActions.loadFilesByBoardFailure({ error }))),
-        ),
-      ),
+      switchMap(({ boardId }) => this.filesService.getFilesByBoard(boardId)),
+      map((files) => FileActions.loadFilesByBoardSuccess({ files })),
     );
   });
 
   uploadFile$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.uploadFile),
-      concatMap(({ boardId, taskId, file, filename }) =>
-        this.filesService.uploadFile(boardId, taskId, file, filename).pipe(
-          map((newFile) => FileActions.uploadFileSuccess({ file: newFile })),
-          catchError((error) => of(FileActions.uploadFileFailure({ error }))),
-        ),
-      ),
+      concatMap(({ boardId, taskId, file, filename }) => this.filesService.uploadFile(boardId, taskId, file, filename)),
+      map((newFile) => FileActions.uploadFileSuccess({ file: newFile })),
     );
   });
 
   deleteFile$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(FileActions.deleteFile),
-      concatMap(({ id }) =>
-        this.filesService.getFilesByBoard(id).pipe(
-          map(() => FileActions.deleteFileSuccess({ id })),
-          catchError((error) => of(FileActions.deleteFileFailure({ error }))),
-        ),
-      ),
+      concatMap(({ id }) => this.filesService.deleteFile(id)),
+      map(({ _id: id }) => FileActions.deleteFileSuccess({ id })),
     );
   });
 
