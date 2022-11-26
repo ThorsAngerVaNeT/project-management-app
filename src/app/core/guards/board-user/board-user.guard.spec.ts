5d8859772d8233b309df0c306d50084c89444e61
Vadzim Antonau <vanet@tut.by>
2022-11-27 00:10:20 +0300

    feat: add notification to board user guard

--- src/app/core/guards/board-user/board-user.guard.spec.ts
+++ src/app/core/guards/board-user/board-user.guard.spec.ts
@@ -1,5 +1,7 @@
+import { Overlay } from '@angular/cdk/overlay';
 import { TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 import { BoardUserGuard } from './board-user.guard';
 
@@ -7,7 +9,7 @@ describe('BoardUserGuard', () => {
   let guard: BoardUserGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [provideMockStore()] });
+    TestBed.configureTestingModule({ providers: [provideMockStore(), NzNotificationService, Overlay] });
     guard = TestBed.inject(BoardUserGuard);
   });
 
--- src/app/core/guards/board-user/board-user.guard.ts
+++ src/app/core/guards/board-user/board-user.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanLoad, Router, UrlTree } from '@angular/router';
 import { concatLatestFrom } from '@ngrx/effects';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { map, Observable } from 'rxjs';
 import { StoreFacade } from '../../services/store-facade/store-facade';
 
@@ -12,12 +13,21 @@ export class BoardUserGuard implements CanActivate, CanLoad {
     concatLatestFrom(() => this.storeFacade.boardDetail$),
     map(([{ _id: userId }, { board }]) => {
       const userIds = board?.users.map((boardUser) => boardUser._id);
-      if (board?.owner._id === userId || userIds?.includes(userId)) return true;
+      if (board?.owner._id === userId || userIds?.includes(userId)) {
+        return true;
+      }
+
+      this.notification.create(
+        'warning',
+        'Access Denied',
+        'You tried to take access to the board where you are not a participant. You were redirected to the Boards Page!',
+      );
+
       return this.router.parseUrl('/boards');
     }),
   );
 
-  constructor(private router: Router, private storeFacade: StoreFacade) {}
+  constructor(private router: Router, private storeFacade: StoreFacade, private notification: NzNotificationService) {}
 
   canActivate(): Observable<boolean | UrlTree> {
     return this.isBoardUser$;
