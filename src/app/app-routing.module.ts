5f1c467332e14ce212276d72f6cb01cea94edfae
Vadzim Antonau <vanet@tut.by>
2022-11-26 23:05:45 +0300

    feat: add AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth/auth.guard';
 import { RootPageGuard } from './core/guards/root-page/root-page.guard';
 
 import { ConfirmationComponent } from './shared/components/confirmation/confirmation.component';
@@ -11,9 +12,15 @@ const routes: Routes = [
     canActivate: [RootPageGuard],
   },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
-  { path: 'boards', loadChildren: () => import('./boards/boards.module').then((m) => m.BoardsModule) },
+  {
+    path: 'boards',
+    loadChildren: () => import('./boards/boards.module').then((m) => m.BoardsModule),
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
+  },
   { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then((m) => m.TasksModule) },
   { path: 'confirm', component: ConfirmationComponent },
+  { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
