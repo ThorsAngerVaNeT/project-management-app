c1078063cb16d438fcb1982ddf8bf16d0979b83c
Vadzim Antonau <vanet@tut.by>
2022-11-27 18:15:51 +0300

    fix: board user guard test

--- src/app/core/guards/board-user/board-user.guard.spec.ts
+++ src/app/core/guards/board-user/board-user.guard.spec.ts
@@ -1,6 +1,7 @@
 import { Overlay } from '@angular/cdk/overlay';
 import { TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 import { BoardUserGuard } from './board-user.guard';
@@ -9,7 +10,10 @@ describe('BoardUserGuard', () => {
   let guard: BoardUserGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [provideMockStore(), NzNotificationService, Overlay] });
+    TestBed.configureTestingModule({
+      providers: [provideMockStore(), NzNotificationService, Overlay],
+      imports: [TranslateModule.forRoot()],
+    });
     guard = TestBed.inject(BoardUserGuard);
   });
 
