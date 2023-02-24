2531c1c16bc2a6cfc1c7cafb1e5f5b9c9ab1df12
Vadzim Antonau <vanet@tut.by>
2023-02-24 13:29:21 +0300

    feat: update API_URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   RETRY_HTTP_COUNT: 3,
-  API_URL: 'https://ng-final-task-backend-production.up.railway.app/',
+  API_URL: 'https://ngpma-be.onrender.com/',
   BOARD_COVER_FILE_TASK_ID: 'board-cover',
   BOARD_COVER_DEFAULT_IMAGE_URL: './assets/images/board-cover.png',
   SEARCH_DEBOUNCE_TIME: 500,
